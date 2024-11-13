63ce1642d1ef286c23ea53849a009a4d505928a6
wasif903 <wasifmehmood903@gmail.com>
2024-11-13 05:54:06 +0500

    Fee Structure Fixed

--- routes/ParentRoutes.js
+++ routes/ParentRoutes.js
@@ -1,15 +1,7 @@
 import {
-  HandleCreateFeeStructure,
-  HandleCreateParent,
-  HandleCreateStudent,
-  HandleGetFeeStatus,
-  HandlePayFee
+  HandleNewAdmission
 } from "../controllers/ParentController.js";
 
-export const ClassRoutes = async (fastify, options) => {
-  fastify.post("/create-parent", HandleCreateParent);
-  fastify.post("/create-student", HandleCreateStudent);
-  fastify.post("/create-fee-structure", HandleCreateFeeStructure);
-  fastify.post("/pay-fee", HandlePayFee);
-  fastify.post("/get-fee-status/:studentId/:year", HandleGetFeeStatus);
+export const ParentRoutes = async (fastify, options) => {
+  fastify.post("/create-new-admission", HandleNewAdmission);
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { authRoutes } from "./AuthRoutes.js";
 import { branchRoutes } from "./BranchRoutes.js";
 import { ClassRoutes } from "./ClassRoutes.js";
+import { ParentRoutes } from "./ParentRoutes.js";
 
 const prefix = '/api';
 
@@ -8,5 +9,6 @@ export const registerRoutes = async (fastify) => {
     fastify.register(authRoutes, { prefix: prefix })
     fastify.register(branchRoutes, { prefix: prefix.concat('/branch') })
     fastify.register(ClassRoutes, { prefix: prefix.concat('/class') })
+    fastify.register(ParentRoutes, { prefix: prefix.concat('/') })
 }
 
