8da7692e39fdca553e2d96b2c0d100e6e8eb0a08
wasif903 <wasifmnehmood903@gmail.com>
2024-12-04 21:40:04 +0500

    Fixing Tracking Bugs

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 import Fastify from "fastify";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import { registerRoutes } from "./routes/index.js";
 import { connectToDatabase } from "./utils/db.js";
 // import fastifySocketIO from "";
+import fastifyCors from "@fastify/cors";
 
 dotenv.config();
 
 const start = async () => {
-    const app = Fastify();
+  const app = Fastify();
 
-    const PORT = process.env.PORT
+  const PORT = process.env.PORT;
 
+  app.register(fastifyCors, {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  });
 
-    await registerRoutes(app);
-    await connectToDatabase();
+  await registerRoutes(app);
+  await connectToDatabase();
 
-    app.listen({ port: PORT, host: '0.0.0.0' }, (err, addr) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log(`Application started on http://localhost:${PORT}`)
-        }
-    })
-}
+  app.listen({ port: PORT, host: "0.0.0.0" }, (err, addr) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Application started on http://localhost:${PORT}`);
+    }
+  });
+};
 
 start();
